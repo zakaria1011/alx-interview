efc0a81bc8005b9a4e3e032dbfe8630cac231f06
zakaria <grarizakaria@gmail.com>
2024-05-02 19:03:12 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" lockboxess"""
+
+
+def canUnlockAll(boxes):
+    """ lockboxes"""
+    visited = [False] * len(boxes)
+    visited[0] = True
+
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+    return all(visited)
