3a5a56053122e118e167d5b297140094e89b8ae7
zakaria <grarizakaria@gmail.com>
2024-06-21 00:02:36 +0100

    alx

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python3
+""" makechange"""
+
+
 def makeChange(coins, total):
+    """ calculate coins needed """
     if total <= 0:
         return 0
-    
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     for coin in coins:
         for j in range(coin, total + 1):
             dp[j] = min(dp[j], dp[j - coin] + 1)
-    
+
     return dp[total] if dp[total] != float('inf') else -1
