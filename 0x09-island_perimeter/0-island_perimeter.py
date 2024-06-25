e4b22c9ad68b47e48d127f8f83bdebfbf34d2de4
zakaria <grarizakaria@gmail.com>
2024-06-26 00:29:41 +0100

    alx

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" island perimetre """
+
+
+def island_perimeter(grid):
+    """ calculate the perimetre"""
+    perimeter = 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == rows - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == cols - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
